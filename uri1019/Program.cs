d98513ced7f4eb3667edb666702738c3c9549401
wesslingpedro <wesslingpedro@gmail.com>
2024-05-08 20:43:38 -0300

    Primeiro commit

---
+++ uri1019/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+
+namespace uri1009
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int N, horas, resto, minutos, segundos;
+
+            N = int.Parse(Console.ReadLine());
+
+            horas = N / 3600;
+            resto = N % 3600;
+
+            minutos = resto / 60;
+            segundos = resto % 60;
+
+            
+
+
+
+
+
+
+
+
+            Console.WriteLine(horas + " : " + minutos + " : " + segundos, CultureInfo.InvariantCulture);
+
+
+
+
+
+
+        }
+    }
+}
